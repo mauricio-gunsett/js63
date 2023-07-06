470e76a5fddfe7c13805b99dbf7e735a28938a45
Alexis Mauricio Gunsett <mau_gunsett@hotmail.com>
2023-07-06 15:52:39 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,31 @@
+"use strict"
+
+// funciones
+console.log ("Hola mundo")
+
+
+
+
+// variables 
+var usuario = "Pepe";
+
+console.log (usuario)
+
+let usuario2 = "mau";
+usuario2 = "ale"
+console.log (usuario2)
+
+const usuarioo = "alexis" 
+console.log (usuarioo);
+
+
+// let respuestaUsuario;
+// respuestaUsuario = prompt ("¿Cual es tu nombre")
+// console.log (respuestaUsuario)
+document.write ("hola")
+
+let respuestaUsuario;
+respuestaUsuario = prompt ("ingrese su nombre")
+alert ("el nombre que ingresaste es: " + respuestaUsuario)
+
+console.log (respuestaUsuario)
